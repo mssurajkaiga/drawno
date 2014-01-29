7023189b60c8bfd306a764c4b566842ebf64d9bd
M S Suraj <mssurajkaiga@gmail.com>
2014-01-29 14:37:21 +0530

    Basic canny edge detection over contours

--- main.cpp
+++ main.cpp
@@ -7,46 +7,19 @@
 using namespace cv;
 using namespace std;
 
-Mat src; Mat src_gray;
-int thresh = 100;
+Mat src; Mat src_gray; Mat src_gray2, src_gray3;
+int thresh = 255;
 int max_thresh = 255;
 RNG rng(12345);
 
-Mat dst, detected_edges;
+Mat dst, detected_edges, drawing;
 
 int edgeThresh = 1;
-int lowThreshold;
+int lowThreshold = 100;
 int const max_lowThreshold = 100;
 int ratio = 3;
 int kernel_size = 3;
 char* window_name = "Edge Map";
-
-/**
- * @function thresh_callback
- */
-void thresh_callback(int, void* )
-{
-  Mat canny_output;
-  vector<vector<Point> > contours;
-  vector<Vec4i> hierarchy;
-
-  /// Detect edges using canny
-  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
-  /// Find contours
-  findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-  /// Draw contours
-  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-  for( size_t i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
-     }
-
-  /// Show in a window
-  namedWindow( "Contours", WINDOW_AUTOSIZE );
-  imshow( "Contours", drawing );
-}
 
 /**
  * @function CannyThreshold
@@ -55,7 +28,7 @@ void thresh_callback(int, void* )
 void CannyThreshold(int, void*)
 {
   /// Reduce noise with a kernel 3x3
-  blur( src_gray, detected_edges, Size(3,3) );
+  blur( src_gray2, detected_edges, Size(3,3) );
 
   /// Canny detector
   Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
@@ -64,7 +37,16 @@ void CannyThreshold(int, void*)
   dst = Scalar::all(0);
 
   src.copyTo( dst, detected_edges);
-  imshow( window_name, dst );
+  cvtColor(dst, src_gray3, CV_BGR2GRAY );
+  unsigned char *input = (unsigned char*)(src_gray3.data);
+    for(int i = 0;i < src_gray3.rows;i++) {
+      for(int j = 0;j < src_gray3.cols;j++) {
+            if (input[src_gray3.step * j + i ] != 0) {
+                input[src_gray3.step * j + i ] = 255;
+            }
+        }
+    }
+  imshow(window_name, src_gray3);
 }
 
 /**
@@ -92,19 +74,31 @@ int main( int argc, char** argv )
   dst.create( src.size(), src.type() );
 
   /// Convert the image to grayscale
-  cvtColor( src, src_gray, CV_BGR2GRAY );
+  drawing = src;
+  cvtColor(drawing, src_gray2, CV_BGR2GRAY );
 
   /// Create a window
   namedWindow( window_name, CV_WINDOW_AUTOSIZE );
 
-  /// Create a Trackbar for user to enter threshold
-  createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+  Mat canny_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  /// Detect edges using canny
+  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+  /// Find contours
+  findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  /// Draw contours
+  drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+  for( size_t i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
+     }
 
   /// Show the image
   CannyThreshold(0, 0);
-
-  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
 
   waitKey(0);
   return(0);
